511207de26f764368ede400524497e81d33d342c
AzzieDev <18339356+AzzieDev@users.noreply.github.com>
2024-03-06 18:59:39 -0500

    Added my animal

---
+++ AnimalImpl/kat953162.java
@@ -0,0 +1,26 @@
+package SeeAndSay.AnimalImpl;
+
+import SeeAndSay.Animal;
+
+public class kat953162 extends Animal {
+
+	@Override
+	public String getAuthor() {
+		return "David Katz";
+	}
+
+	@Override
+	public String getAnimalName() {
+		return "Chameleon";
+	}
+
+	@Override
+	public String getSound() {
+		return "This is the sound a chameleon makes";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Lizardy";
+	}
+}
